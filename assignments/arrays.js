13b17a5620c7c8c81d6b0cf9dfe1dd1a74b14961
pranith9810 <n.rao@mountblue.tech>
2020-07-15 12:04:29 +0530

    completed all functions except two

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -15,21 +15,36 @@ function each(elements, cb) {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for(let i=0;i<elements.length;i++){
+      elements[i]=cb(elements[i]);
+  }
+
 }
+
 
 function map(elements, cb) {
   // Do NOT use .map, to complete this function.
   // How map works: Map calls a provided callback function once for each element in an array, in order, and functionructs a new array from the res .
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  let newArray = []
+  for(let i=0;i<elements.length;i++){
+    newArray.push(cb(elements[i]));
+}
+return newArray;
 }
 
-function reduce(elements, cb, startingValue) {
+function reduce(elements, cb, startingValue=0) {
   // Do NOT use .reduce to complete this function.
   // How reduce works: A reduce function combines all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let accumulator =0;
+  for(let i=startingValue;i<elements.length;i++){
+    accumulator = cb(accumulator,elements[i]);
+  }
+  return accumulator;
 }
 
 function find(elements, cb) {
@@ -37,13 +52,53 @@ function find(elements, cb) {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for(let i=0;i<elements.length;i++){
+      if(cb(elements[i])){
+          return elements[i];
+      }
+  }
+  return undefined;
 }
 
 function filter(elements, cb) {
   // Do NOT use .filter, to complete this function.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  let newArray=[]
+  for(let i=0;i<elements.length;i++){
+      if(cb(elements[i])){
+          newArray.push(elements[i]);
+      }
+  }
+  return newArray;
 }
+
+//Callback Functions
+
+const multiplyByTen = num => num*10;
+
+const adder = (accumulator,currentValue) => accumulator+currentValue;
+
+const checkIfDivBy20 = num => num%20 === 0;
+
+//Outputs for each function 
+
+each(items,multiplyByTen);
+console.log("Output for Each function: "+items);
+
+let mapAnsArray = []
+mapAnsArray = map(items,multiplyByTen);
+console.log("Output for map function: "+mapAnsArray);
+
+let reduceAns =reduce(items,adder,1);
+console.log("Output for reduce function: "+reduceAns)
+
+let findAns = find(items,checkIfDivBy20);
+console.log("Output for find function: "+findAns);
+
+let filterAnsArray = []
+filterAnsArray = filter(items,checkIfDivBy20);
+console.log("Output for filter function: "+filterAnsArray);
 
 const nestedArray = [1, [2], [[3]], [[[4]]]]; // use this to test 'flatten'
 
@@ -51,4 +106,6 @@ function flatten(elements) {
   // Flattens a nested array (the nesting can be to any depth).
   // Hint: You can solve this using recursion.
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+
+
 }
--- assignments/closure.js
+++ assignments/closure.js
@@ -2,12 +2,32 @@ function counterFactory() {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let counter =0;
+  return{
+    increment(){
+    counter++;
+    return counter;
+    },
+    decrement(){
+    counter--;
+    return counter;
+    }
+  }
 }
 
 function limitFunctionCallCount(cb, n) {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
   // Returning null is acceptable if cb can't be returned
+
+  return function(){
+    if(n!=0){
+    n--;
+    return cb();
+    }
+    else 
+    return null;
+  };
 }
 
 function cacheFunction(cb) {
@@ -17,4 +37,21 @@ function cacheFunction(cb) {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+
 }
+
+//1st function output
+const count = counterFactory();
+count.increment();
+count.increment();
+count.decrement();
+console.log(count.increment());
+
+//2nd function output
+const returnHello = () => 'hello';
+const limitFunc = limitFunctionCallCount(returnHello,2);
+console.log(limitFunc());
+console.log(limitFunc());
+console.log(limitFunc());
+
+
